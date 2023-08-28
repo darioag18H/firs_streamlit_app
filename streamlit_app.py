269d3ca8a41a59a9a8377ff720df2da18d331afd
darioag18H <143029632+darioag18H@users.noreply.github.com>
2023-08-28 15:20:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,6 @@ import requests
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
 
 
 # Streamlit code
@@ -34,6 +32,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # normalized watermelon json 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display the table to watermelon
