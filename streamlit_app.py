c3d1dd6f424394e7a131b3fea7bec7cd64f3bf9f
darioag18H <143029632+darioag18H@users.noreply.github.com>
2023-08-28 15:06:10 -0600

    Update streamlit_app.py
    added new library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,17 @@
 # Libraries
 import streamlit
 import pandas
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+
 
 # Variables
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
 # Streamlit code
