a7e638401658b995cec7bc220d62b2caea6c2610
darioag18H <143029632+darioag18H@users.noreply.github.com>
2023-08-28 11:12:54 -0600

    Update streamlit_app.py
    setted my_fruit_list index

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
+# Libraries
 import streamlit
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+# Variables
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Streamlit code
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
